6abb8ed9b425857542ccef97a63e1eb8ced7795c
mariusvika <mariuv16@student.uia.no>
2017-03-03 11:39:37 +0100

    Forbedringer i koden i oppgave 1

--- Oppgave 1/main.go
+++ Oppgave 1/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	//"IntelliJ/ICA03/ascii"
 	"./ascii"
 )
 
 func main() {
-	//var temp string;
-	//temp = ">";
-	//ascii.IterateOverASCIIStringLiteral(temp)
+	var temp string;
+	temp = "?";
+	ascii.IterateOverASCIIStringLiteral(temp)
 	ascii.GreetingASCII()
 }
