92bfeacc0be0271c327f965fb26bb1fc093f8a95
mariusvika <mariuv16@student.uia.no>
2017-03-03 11:33:28 +0100

    Revert "Forbedringer til koden i oppgave 1"
    This reverts commit af3fa9691165aaee8ca1dfe2cbacfa22e40533f0.

--- Oppgave 1/ascii/ascii.go
+++ Oppgave 1/ascii/ascii.go
@@ -10,30 +10,42 @@ const ascii = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f"
 	`@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_` +
 	"`abcdefghijklmnopqrstuvwxyz{|}~\x7f"
 
-//noinspection GoUnusedExportedFunction
+// Funksjon tar en streng med kun ASCII tegn og lager en utskrift på
+// følgende format:
+// [ascii-kode heksadesimalt med store bokstaver A-F][mellomrom]
+// [symbol for ascii-kode][mellomrom][ascii-kode binært][linjeskift]
+// Eksempel (bruk denne funksjonen i en main.go fil):
+//	…
+// 3E > 111110
+// 3F ? 111111
+// 40 @ 1000000
+// ...
 func IterateOverASCIIStringLiteral(sl string) {
 	// Kode for Oppgave 1
+	//fmt.Println("" + " " + "" + " " + "");
+
 	for i := 0; i < len(sl); i ++ {
+		fmt.Print("Hexa = ")
+		fmt.Printf("% X\n", sl)
+		//fmt.Println()
 
-		//Correct format
-		fmt.Println("Hexa/symbol/binært:")
-		fmt.Printf ("%X %s %b\n", sl, sl, sl[i])
+		fmt.Println("Symbol / input = " + sl)
 
-		//Old code
-		//fmt.Print("Hexa   =   ")
-		//fmt.Printf("% X\n", sl, sl)
-
-		//fmt.Println("Symbol =   " + sl)
-
-		//fmt.Print("Binært =   ")
-		//fmt.Printf("%b\n", sl[i])
+		fmt.Print("Binært = ")
+		fmt.Printf("%b\n", sl[i])
 	}
 }
 
-//noinspection GoUnusedExportedFunction
+// Funksjonen skal generere en utskrift fra en sekvens av bytes,
+// dvs. av typen []bytes (det betyr at du må finne den heksadesimale
+// eller binære representasjonen av alle tegn i strengen,
+// som skal skrives ut (inkludert anførselstegn eller
+// “double quotes” på engelsk).
+// Funksjonen greetingASCII() returnerer en variabel av typen string,
+// som inneholder kun ASCII tegn (ikke utvidet ASCII).
+// Gjelder oppgave 1b
 func GreetingASCII() {
-	//"Hello :-)" i hexa
-	s := "\x22\x48\x65\x6C\x6C\x6F\x20\x3A\x2D\x29\x22"
-	fmt.Println()
-	fmt.Println(s)
+	//Hello :-) i hexa
+	s := "\x48\x65\x6C\x6C\x6F\x20\x3A\x2D\x29"
+		fmt.Println(s)
 }
