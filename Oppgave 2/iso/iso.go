2367e00a1df48974e0def0469c795b8f4a422d35
Christoffer Lislevand <chrirl16@student.uia.no>
2017-03-07 20:50:37 +0100

    Initial commit - legger inn mal for oppgave 2

---
+++ Oppgave 2/iso/iso.go
@@ -0,0 +1,17 @@
+package iso
+
+//noinspection GoUnusedExportedFunction
+func AddExtendedAscii() string{
+
+}
+
+//noinspection GoUnusedExportedFunction
+func IterateOverExtendedASCIIStringLiteral(sl string) {
+	// Kode for Oppgave 2a
+}
+
+//noinspection GoUnusedExportedFunction
+func GreetingExtendedASCII() string{
+	// Kode for Oppgave 2b
+
+}
---
+++ Oppgave 2/test/iso_test.go
@@ -0,0 +1,13 @@
+package test
+
+import (
+	"testing"
+)
+
+func TestGreetingExtendedASCII(t *testing.T) {
+
+}
+
+func isExtendedASCII(s string) bool {
+
+}
