00ed34d3310a93c5c19c6471b04228000755976d
joakim16 <joakim16@uia.no>
2017-03-07 21:08:14 +0100

    Oppgave 2 - la til koden for oppgave 2

--- Oppgave 2/iso/iso.go
+++ Oppgave 2/iso/iso.go
@@ -1,17 +1,40 @@
 package iso
 
+import "fmt"
+
 //noinspection GoUnusedExportedFunction
 func AddExtendedAscii() string{
+	var ExtendedAscii []byte
+	for i := 0x80; i <= 0xFF; i ++ {
+		ExtendedAscii = append(ExtendedAscii, byte(i))
+	}
 
+	str := string(ExtendedAscii)
+	//fmt.Printf("%q", str)
+	return str
 }
 
 //noinspection GoUnusedExportedFunction
 func IterateOverExtendedASCIIStringLiteral(sl string) {
 	// Kode for Oppgave 2a
+	fmt.Println("Extended Ascii")
+	fmt.Println("Heksa/symbol/binært:")
+	for i := 0; i < len(sl); i ++ {
+		fmt.Printf("%X %c %b \n", sl[i], sl[i], sl[i])
+	}
+	fmt.Println()
 }
 
 //noinspection GoUnusedExportedFunction
 func GreetingExtendedASCII() string{
 	// Kode for Oppgave 2b
+	greeting := []byte{34, 83, 97, 108, 117, 116, 44, 32, 231, 97, 32, 118, 97, 32, 176, 45, 41, 32, 128, 53, 48, 34}
+	for i := 0; i < len(greeting); i++ {
+		fmt.Printf("%c", greeting[i])
+	}
+
+	//String converter
+	str := string(greeting)
+	return str
 
 }
--- Oppgave 2/test/iso_test.go
+++ Oppgave 2/test/iso_test.go
@@ -2,12 +2,21 @@ package test
 
 import (
 	"testing"
+	"../iso"
 )
 
 func TestGreetingExtendedASCII(t *testing.T) {
-
+	testExtendedAscii := iso.GreetingExtendedASCII()
+	if !(isExtendedASCII(testExtendedAscii)){
+		t.Fail()
+	}
 }
 
 func isExtendedASCII(s string) bool {
-
+	for _, c := range s {
+		if c < 127 && c > 255 {
+			return false
+		}
+	}
+	return true
 }
