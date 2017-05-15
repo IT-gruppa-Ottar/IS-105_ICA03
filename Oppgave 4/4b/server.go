8d04c4cddce46102681edacadc48431976784a3f
Christoffer Lislevand <chrirl16@student.uia.no>
2017-05-15 19:48:54 +0200

    Fikset klokke

--- Oppgave 4/4b/server.go
+++ Oppgave 4/4b/server.go
@@ -1,50 +1,47 @@
 // Filen for å eksperimentere med webserver i ICA03 (IS-105)
-// Starte server med
-// 			go run server.go
-// Du kan ha tilgang til server fra nettleser med
-// 			http://localhost:3000
-//
+// Starte server med go run server.go
+// Du kan ha tilgang til server fra nettleser med http://localhost:3000
 package main
 
 import (
 	"net/http"
 	"time"
-
 	"fmt"
 )
 
 func main() {
+	//getTime()
 	http.HandleFunc("/", foo)
 	http.ListenAndServe(":3000", nil)
-	Now()
 }
 
 func foo(w http.ResponseWriter, r *http.Request) {
-	t := time.Now()
-	fmt.Println(t.Format("20060102150405"))
+	//fmt.Println(t.Format("2006 01 02 15 04 05"))
 	//w.Write([]byte("20060102150405"))
+
 	// Her kan man bl. a. skrive data som skal sendes til nettleser
 	// Neste linje kan brukes for å endre koding av sekvensen som sendes til nettleser
+
 	// Standardinstilling er UTF-8
 	//w.Header().Set("Content-Type", "text/html;charset=ISO-8859-1")
 
 	// Her skriver man data som er respons til brukeren som har skrevet
 	// http://localhost:3000 i sin nettleser
-	w.Write([]byte("<font color=\"pink\">Hvordan g\xe5r det, <b>\u16a6</b> ?</font><br/>"))
+	w.Write([]byte("<font color=\"black\">Hvordan g\xe5r det, <b>\u16a6</b> ?</font><br/>"))
 	w.Write([]byte("\u16a6 - Thurs<br/>"))
-	w.Write([]byte("\xE2\x8F\xB0"))
+	w.Write([]byte("\xE2\x8F\xB0\n"))
 
-
-	//Type time
-	//Konvertere
-
-	//fmt.Printf("%T\n", t)
-	//fmt.Sprintf("%s", t)
-	
+	klokke := getTime()
+	w.Write([]byte(klokke))
 }
 
-func Now() {
-	//t := time.Now()
+func getTime()[]byte{
+	//Henter tid
+	t := time.Now()
 
-	//fmt.Println(t.Format("20060102150405"))
+	//Konverterer til byte
+	test := fmt.Sprintf("%s", t)
+	temp := []byte (test)
+	
+	return temp
 }
