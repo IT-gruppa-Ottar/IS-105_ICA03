5b7876f1ccb309a64c0b4a6a20f0dfd9fe684ec1
Christoffer Lislevand <chrirl16@student.uia.no>
2017-05-18 14:38:55 +0200

    Fikser på server.go

--- Oppgave 4/4b/server.go
+++ Oppgave 4/4b/server.go
@@ -15,21 +15,14 @@ func main() {
 	http.ListenAndServe(":3000", nil)
 }
 
+/**
+Her kan man bl. a. skrive data som skal sendes til nettleser
+Neste linje kan brukes for å endre koding av sekvensen som sendes til nettleser
+Her skriver man data som er respons til brukeren som har skrevet
+http://localhost:3000 i sin nettleser
+ */
 func foo(w http.ResponseWriter, r *http.Request) {
-	//fmt.Println(t.Format("2006 01 02 15 04 05"))
-	//w.Write([]byte("20060102150405"))
-
-	// Her kan man bl. a. skrive data som skal sendes til nettleser
-	// Neste linje kan brukes for å endre koding av sekvensen som sendes til nettleser
-
-	// Standardinstilling er UTF-8
-	//w.Header().Set("Content-Type", "text/html;charset=ISO-8859-1")
-
-	// Her skriver man data som er respons til brukeren som har skrevet
-	// http://localhost:3000 i sin nettleser
-	w.Write([]byte("<font color=\"black\">Hvordan g\xe5r det, <b>\u16a6</b> ?</font><br/>"))
-	w.Write([]byte("\u16a6 - Thurs<br/>"))
-	w.Write([]byte("\xE2\x8F\xB0\n"))
+	w.Write([]byte("<font color=\"black\">\xE2\x8F\xB0\t</font><br/>"))
 
 	klokke := getTime()
 	w.Write([]byte(klokke))
